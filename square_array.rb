f2821014ecae63332818914b9c55efc4334d3c36
hao wang <ge_nah@hotmail.com>
2019-04-08 21:15:11 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,4 @@
 def square_array(array)
   # your code here
-  new_array = []
-  array.each do |ele|
-    new_array << ele * ele
-  end
-  return new_array
+  return array.collect { |ele| ele * ele}
 end
